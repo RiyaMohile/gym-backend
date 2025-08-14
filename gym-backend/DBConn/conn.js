2304df39fd14c32b8ef0c9183f68ca0adad897b4
riyamohile812@gmail.com <riyamohile812@gmail.com>
2025-08-14 17:49:15 +0530

    url

--- gym-backend/DBConn/conn.js
+++ gym-backend/DBConn/conn.js
@@ -1,30 +1,15 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// mongoose
-//   .connect("mongodb://localhost:27017/gymBackend")
-//   // netlify
-//   // .connect("https://gym-backend-iota.vercel.app/")
-//   .then(() => console.log("db connected"))
-//   .catch((err) => {
-//     console.log(err);
-//   });
+mongoose
+  .connect("mongodb://localhost:27017/gymBackend")
+  // netlify
+  // .connect("https://gym-backend-iota.vercel.app/")
+  .then(() => console.log("db connected"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL);
-
-// mongoose.connect("mongodb://localhost:27017/Market");
-
-const connection = mongoose.connection;
-
-connection.on('connected',()=>{
-    console.log('mongo DB connection successful')
-})
-
-connection.on('error',(err)=>{
-    console.log('mongo DB connection failed')
-})
-module.exports = connection;
 
 
